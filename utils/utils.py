561ee3adebff49e58b8e996dd9b6454528239bd7
apeuvot <arthur.peuvot@cea.fr>
2024-12-10 11:54:52 +0100

    fill gitignore, clean utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -8,187 +8,6 @@ from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 from utils.ontology import *
 
-SUBJECT_RELATION_OBJECT_TABLE = [
-    ("ACTOR, EVENT, PLACE", "IS_LOCATED_IN", "PLACE"),
-    ("ACTOR, PLACE", "IS_OF_NATIONALITY", "NATIONALITY"),
-    ("ACTOR", "CREATED", "ORGANIZATION"),
-    ("ACTOR", "HAS_CONTROL_OVER", "ACTOR, MATERIAL, PLACE"),
-    ("ACTOR", "INITIATED", "EVENT"),
-    ("ACTOR", "IS_AT_ODDS_WITH", "ACTOR"),
-    ("ACTOR", "IS_COOPERATING_WITH", "ACTOR"),
-    ("ACTOR", "IS_IN_CONTACT_WITH", "ACTOR"),
-    ("ACTOR", "IS_PART_OF", "ORGANIZATION"),
-    ("EVENT", "DEATH_NUMBER", "QUANTITY"),
-    ("EVENT", "END_DATE", "TIME"),
-    ("EVENT", "HAS_CONSEQUENCE", "EVENT"),
-    ("EVENT", "INJURED_NUMBER", "QUANTITY"),
-    ("EVENT", "START_DATE", "TIME"),
-    ("EVENT", "STARTED_IN", "PLACE"),
-    ("MATERIAL", "HAS_COLOR", "COLOR"),
-    ("MATERIAL", "HAS_FOR_HEIGHT", "HEIGHT"),
-    ("MATERIAL", "HAS_FOR_LENGTH", "LENGTH"),
-    ("MATERIAL", "HAS_FOR_WIDTH", "WIDTH"),
-    ("MATERIAL", "HAS_QUANTITY", "QUANTITY"),
-    ("MATERIAL", "IS_REGISTERED_AS", "MATERIAL_REFERENCE"),
-    ("MATERIAL", "WEIGHS", "WEIGHT"),
-    ("ORGANIZATION", "CREATED_IN", "TIME"),
-    ("ORGANIZATION", "DISSOLVED_IN", "TIME"),
-    ("ORGANIZATION", "IS_OF_SIZE", "QUANTITY"),
-    ("ORGANIZATION", "OPERATES_IN", "PLACE"),
-    ("PERSON", "DIED_IN", "EVENT"),
-    ("PERSON", "HAS_CATEGORY", "CATEGORY"),
-    ("PERSON", "HAS_FAMILY_RELATIONSHIP", "PERSON"),
-    ("PERSON", "HAS_FIRST_NAME", "FIRSTNAME"),
-    ("PERSON", "HAS_GENDER_FEMALE", "N/A"),
-    ("PERSON", "HAS_GENDER_MALE", "N/A"),
-    ("PERSON", "HAS_LAST_NAME", "LASTNAME"),
-    ("PERSON", "IS_BORN_IN", "PLACE"),
-    ("PERSON", "IS_BORN_ON", "TIME"),
-    ("PERSON", "IS_DEAD_ON", "TIME"),
-    ("PERSON", "RESIDES_IN", "PLACE"),
-    ("PLACE", "HAS_LATITUDE", "LATITUDE"),
-    ("PLACE", "HAS_LONGITUDE", "LONGITUDE"),
-]
-
-
-SUBTYPE_TO_GENERAL = {
-    # Actor types
-    "GOVERNMENT_ORGANIZATION": "ORGANIZATION",
-    "MILITARY_ORGANISATION": "ORGANIZATION",
-    "NON_MILITARY_GOVERNMENT_ORGANISATION": "ORGANIZATION",
-    "GROUP_OF_INDIVIDUALS": "ORGANIZATION",
-    "INTERGOVERNMENTAL_ORGANISATION": "ORGANIZATION",
-    "NON_GOVERNMENTAL_ORGANISATION": "ORGANIZATION",
-    "CIVILIAN": "PERSON",
-    "TERRORIST_OR_CRIMINAL": "PERSON",
-    "MILITARY": "PERSON",
-    
-    # Organization maps to Actor
-    "ORGANIZATION": "ACTOR",
-    "PERSON": "ACTOR",
-    
-    # Event types
-    "ACCIDENT": "EVENT",
-    "CBRN_EVENT": "EVENT",
-    "AGITATING_TROUBLE_MAKING": "EVENT",
-    "CIVIL_WAR_OUTBREAK": "EVENT",
-    "COUP_D_ETAT": "EVENT",
-    "DEMONSTRATION": "EVENT",
-    "ELECTION": "EVENT",
-    "GATHERING": "EVENT",
-    "ILLEGAL_CIVIL_DEMONSTRATION": "EVENT",
-    "NATURAL_CAUSES_DEATH": "EVENT",
-    "RIOT": "EVENT",
-    "STRIKE": "EVENT",
-    "SUICIDE": "EVENT",
-    "BOMBING": "EVENT",
-    "CRIMINAL_ARREST": "EVENT",
-    "DRUG_OPERATION": "EVENT",
-    "HOOLIGANISM_TROUBLEMAKING": "EVENT",
-    "POLITICAL_VIOLENCE": "EVENT",
-    "THEFT": "EVENT",
-    "TRAFFICKING": "EVENT",
-    "ECONOMICAL_CRISIS": "EVENT",
-    "EPIDEMIC": "EVENT",
-    "FIRE": "EVENT",
-    "NATURAL_EVENT": "EVENT",
-    "POLLUTION": "EVENT",
-    
-    # Quantity attributes
-    "QUANTITY_EXACT": "QUANTITY",
-    "QUANTITY_FUZZY": "QUANTITY",
-    "QUANTITY_MAX": "QUANTITY",
-    "QUANTITY_MIN": "QUANTITY",
-
-    # Time attributes
-    "TIME_EXACT": "TIME",
-    "TIME_FUZZY": "TIME",
-    "TIME_MAX": "TIME",
-    "TIME_MIN": "TIME"
-}
-
-SUBJECT_TYPES = ['ACTOR', 'PERSON', 'ORGANIZATION', 'EVENT', 'PLACE', 'MATERIAL']
-OBJECT_TYPES = ['PLACE', 'NATIONALITY', 'ORGANIZATION', 'ACTOR', 'MATERIAL', 'EVENT', 'QUANTITY', 'TIME', 
-                'COLOR', 'HEIGHT', 'LENGTH', 'WIDTH', 'MATERIAL_REFERENCE', 'WEIGHT', 'CATEGORY', 'PERSON', 
-                'FIRSTNAME', 'N/A', 'LASTNAME', 'LATITUDE', 'LONGITUDE']
-
-
-def lower_case_str(item):
-    # Si l'item est une chaîne, on la met en minuscule et on remplace les underscores
-    if isinstance(item, str):
-        return item.lower().replace('_', ' ')
-    return item
-
-def transform_relations(relations):
-    return [[lower_case_str(i) for i in relation] for relation in relations]
-
-def transform_entities(entities):
-    for entity in entities:
-        if 'type' in entity:
-            entity['type'] = lower_case_str(entity['type'])
-    return entities
-
-def lower_case_labels(row):
-    row['relations'] = transform_relations(row['relations'])
-    row['entities'] = transform_entities(row['entities'])
-    return row
-
-def get_subject_relation_object_dicts(lower_case_labels=False):
-
-    # Création du premier objet
-    subject_to_relations_dict = defaultdict(list)
-
-    # Création du deuxième objet
-    subject_object_to_relations_dict = defaultdict(list)
-
-    for subjects, relation, objects in SUBJECT_RELATION_OBJECT_TABLE:
-        # Diviser les types de sujets et d'objets multiples
-        SUBJECT_TYPES = [s.strip() for s in subjects.split(",")]
-        OBJECT_TYPES = [o.strip() for o in objects.split(",")]
-
-        # Transformer les relations si nécessaire
-        if lower_case_labels:
-            relation = lower_case_str(relation)
-
-        # Remplir le premier dictionnaire
-        for subject in SUBJECT_TYPES:
-            if lower_case_labels:
-                subject = lower_case_str(subject)
-            if relation not in subject_to_relations_dict[subject]:
-                subject_to_relations_dict[subject].append(relation)
-
-        # Remplir le deuxième dictionnaire
-        for subject in SUBJECT_TYPES:
-            if lower_case_labels:
-                subject = lower_case_str(subject)
-            for obj in OBJECT_TYPES:
-                if lower_case_labels:
-                    obj = lower_case_str(obj)
-                if relation not in subject_object_to_relations_dict[(subject, obj)]:
-                    subject_object_to_relations_dict[(subject, obj)].append(relation)
-
-    # Conversion de defaultdict en dict pour un usage plus général
-    subject_to_relations_dict = dict(subject_to_relations_dict)
-    subject_object_to_relations_dict = dict(subject_object_to_relations_dict)
-    return subject_to_relations_dict, subject_object_to_relations_dict
-
-
-def lowercase_lists(subtype_to_general, subject_types, object_types):
-    # Transformer le dictionnaire SUBTYPE_TO_GENERAL
-    transformed_subtype_to_general = {lower_case_str(k): lower_case_str(v) for k, v in subtype_to_general.items()}
-
-    # Transformer les listes SUBJECT_TYPES et OBJECT_TYPES
-    transformed_subject_types = [lower_case_str(s) for s in subject_types]
-    transformed_object_types = [lower_case_str(o) for o in object_types]
-
-    return transformed_subtype_to_general, transformed_subject_types, transformed_object_types
-
-
-def create_directory_if_not_exists(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)  
-
 def load_data(data_path):
     df = pd.read_csv(data_path)
     df = df.set_index("id")
@@ -197,36 +16,9 @@ def load_data(data_path):
         df.relations = df.relations.apply(json.loads)
     return df
 
-# Fonction pour obtenir les occurrences et proportions des classes dans chaque split
-def get_split_proportion_per_class(train_df, dev_df, test_df, classes):
-    # Calculer les occurrences totales par classe dans l'ensemble des données
-    total_counts = {cls: train_df[cls].sum() + dev_df[cls].sum() + test_df[cls].sum() for cls in classes}
-
-    # Calculer les occurrences et proportions pour chaque split
-    splits = {'Train': train_df, 'Dev': dev_df, 'Test': test_df}
-    results = {}
-
-    for split_name, df_split in splits.items():
-        split_counts = {cls: df_split[cls].sum() for cls in classes}  # Occurrences par classe dans le split
-        split_proportions = {cls: (split_counts[cls] / total_counts[cls] * 100) if total_counts[cls] > 0 else 0
-                            for cls in classes}  # Proportions par classe
-
-        results[split_name] = split_proportions
-
-    # Affichage des résultats
-    for cls in classes:
-        print(f"\nClasse '{cls}':")
-        for split_name in splits.keys():
-            print(f" - {split_name}: {results[split_name][cls]:.2f}% des occurrences")
-
 def remove_class_columns(df, all_classes):
     return df.drop(columns=all_classes)
     
-# def split_data(df):
-#     train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
-#     dev_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
-#     return train_df, dev_df, test_df
-
 def split_data(df):
     all_classes = sorted(list(set(relation[1] for relations in df['relations'] for relation in relations)))
 
@@ -242,46 +34,12 @@ def split_data(df):
     dev_idx, test_idx = next(msss_temp.split(temp_df, temp_df[all_classes].values))
     dev_df, test_df = temp_df.iloc[dev_idx], temp_df.iloc[test_idx]
 
-    # # Appeler la fonction pour obtenir les proportions
-    # get_split_proportion_per_class(train_df, dev_df, test_df, all_classes)
-
     train_df= remove_class_columns(train_df, all_classes)
     dev_df = remove_class_columns(dev_df, all_classes)
     test_df = remove_class_columns(test_df, all_classes)
 
     return train_df, dev_df, test_df
 
-def check_if_mentions_in_sentence(sentence, mention_1, mention_2):
-    sent_start = sentence.start_char
-    sent_end = sentence.end_char
-    # Vérifier si mention_1 et mention_2 sont dans cette phrase
-    if mention_1['start'] >= sent_start and mention_1['end'] <= sent_end and mention_2['start'] >= sent_start and mention_2['end'] <= sent_end:
-        return True
-    else:
-        return False
-    
-
-    
-# def get_potential_relations(entity_1, entity_2, relation):
-#     original_subject_type = entity_1['type']
-#     original_object_type = entity_2['type']
-#     # types can be part of a more general type (those present in subject_object_to_relations_dict)
-#     # relations are constructed with more and more general types, starting from the most specific types (those presents in example['entities']) 
-#     subject_type = original_subject_type
-#     potential_relation_types_list = []
-#     while subject_type:
-#         if subject_type in SUBJECT_TYPES:
-#             object_type = original_object_type
-#             while object_type:
-#                 if object_type in OBJECT_TYPES:
-#                     potential_relation_types = subject_object_to_relations_dict.get((subject_type, object_type))
-#                     if potential_relation_types is not None:
-#                         potential_relation_types_list.append(potential_relation_types)
-#                 object_type = SUBTYPE_TO_GENERAL.get(object_type)
-#         subject_type = SUBTYPE_TO_GENERAL.get(subject_type)
-#     potential_relation_types_list = [item for sublist in potential_relation_types_list for item in sublist]
-#     return potential_relation_types_list
-    
 def get_potential_relations(entity_1, entity_2):
     if isinstance(entity_1, list):
         subject_type = entity_1[0]['type']
@@ -297,24 +55,6 @@ def get_potential_relations(entity_1, entity_2):
             potential_relations.append(relation[1])
             
     return potential_relations
-
-# def check_if_relation_is_possible(entity_1, entity_2):
-#     original_subject_type = entity_1['type']
-#     original_object_type = entity_2['type']
-#     # types can be part of a more general type (those present in subject_object_to_relations_dict)
-#     # relations are constructed with more and more general types, starting from the most specific types (those presents in example['entities']) 
-#     subject_type = original_subject_type
-#     while subject_type:
-#         if subject_type in SUBJECT_TYPES:
-#             object_type = original_object_type
-#             while object_type:
-#                 if object_type in OBJECT_TYPES:
-#                     potential_relation_types = subject_object_to_relations_dict.get((subject_type, object_type))
-#                     if potential_relation_types is not None:
-#                         return True
-#                 object_type = SUBTYPE_TO_GENERAL.get(object_type)
-#         subject_type = SUBTYPE_TO_GENERAL.get(subject_type)
-#     return False
 
 def check_if_relation_is_possible(entity_1, entity_2):
     subject_type = entity_1['type']
